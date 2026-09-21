f0d0ceda49be676ac499f4999161fbf369eeb45d
aryansaeed8218-gif <aryansaeed8218@gmail.com>
2026-09-21 17:49:43 +0500

    Initial Commit

--- app.py
+++ app.py
@@ -589,7 +589,7 @@ def gemini_analysis(result):
         )
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             contents=prompt,
         )
 
